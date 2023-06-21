54ae342cba31398fee334dfa1b18783e4fb0430e
Shindou Mihou <git@mihou.pw>
2023-06-21 11:21:37 +0800

    feat(burp): add support for `RandN` function

--- burper/functions/random.go
+++ burper/functions/random.go
@@ -4,6 +4,7 @@ import (
 	"burp/burper"
 	"burp/utils"
 	"github.com/dchest/uniuri"
+	"math/rand"
 	"strconv"
 )
 
@@ -18,5 +19,19 @@ var _ = burper.Add(burper.Function{
 			return nil, err
 		}
 		return []byte(uniuri.NewLen(int(length))), nil
+	},
+})
+
+var _ = burper.Add(burper.Function{
+	Name: "randn",
+	Transformer: func(call *burper.Call, tree *burper.Tree) ([]byte, error) {
+		if len(call.Args) < 1 {
+			return nil, burper.CreateMissingArgumentError(call, utils.Array("bound", "uint64"))
+		}
+		length, err := strconv.ParseInt(call.Args[0], 10, 16)
+		if err != nil {
+			return nil, burper.CreateError(call, err.Error())
+		}
+		return []byte(strconv.FormatInt(rand.Int63n(length), 10)), nil
 	},
 })
