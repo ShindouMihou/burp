00b1e1d42b4b113eedc20b88584f65904bb4f409
Shindou Mihou <git@mihou.pw>
2023-06-21 09:54:57 +0800

    feat(burp.toml): allow destructuring

---
+++ services/model.go
@@ -0,0 +1,33 @@
+package services
+
+type Dependency struct {
+	Name  *string `toml:"name" json:"name"`
+	Image string  `toml:"image" json:"image"`
+	Container
+}
+
+type Container struct {
+	Command     *string           `toml:"command" json:"command"`
+	Ports       [][]uint16        `toml:"ports" json:"ports"`
+	Volumes     [][]string        `toml:"volumes" json:"volumes"`
+	Networks    []string          `toml:"networks" json:"networks"`
+	Devices     []string          `toml:"device" json:"devices"`
+	Environment map[string]string `toml:"environment" json:"environment"`
+}
+
+type Service struct {
+	Name  string `toml:"name" json:"name"`
+	Build string `toml:"build" json:"build"`
+	Container
+}
+
+type Burp struct {
+	Service      Service      `toml:"service" json:"service"`
+	Dependencies []Dependency `toml:"dependencies" json:"dependencies"`
+	Environment  *Environment `toml:"environment" json:"environment"`
+}
+
+type Environment struct {
+	File         string            `toml:"file" json:"file"`
+	Replacements map[string]string `toml:"replacements" json:"replacements"`
+}
