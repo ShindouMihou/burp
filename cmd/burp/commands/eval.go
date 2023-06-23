b96ec3969d3c0053ec0f51e5fc9ee3f891e071cc
Shindou Mihou <git@mihou.pw>
2023-06-23 22:10:12 +0800

    feat(cli): add `eval` command

---
+++ cmd/burp/commands/eval.go
@@ -0,0 +1,29 @@
+package commands
+
+import (
+	"burp/internal/burper"
+	"burp/pkg/console"
+	"fmt"
+	"github.com/ttacon/chalk"
+	"github.com/urfave/cli/v2"
+)
+
+var Eval = &cli.Command{
+	Name:        "eval",
+	Description: "Evaluates the given argument with the Burper engine.",
+	Action: func(ctx *cli.Context) error {
+		arg := ctx.Args().First()
+		if arg == "" {
+			fmt.Println(chalk.Red, "૮₍˃⤙˂₎ა", chalk.Reset, "You need to supply the ", console.Highlight, "text", chalk.Reset, "to evaluate!")
+			return nil
+		}
+		tree, err := burper.FromString(arg)
+		if err != nil {
+			fmt.Println(chalk.Red, "(◞‸◟；)", chalk.Reset, "Failed to decode the secret nuclear codes!")
+			fmt.Println(chalk.Red, err.Error())
+			return nil
+		}
+		fmt.Println(tree.String())
+		return nil
+	},
+}
--- cmd/burp/commands/root.go
+++ cmd/burp/commands/root.go
@@ -5,7 +5,7 @@ import "github.com/urfave/cli/v2"
 var App = &cli.App{
 	Name:        "burp",
 	Description: "Deploying smaller applications shouldn't be crazy complicated.",
-	Commands:    []*cli.Command{Login, Logout, Hash, Deploy, Start, Stop, Remove, Restart, Here},
+	Commands:    []*cli.Command{Login, Logout, Hash, Deploy, Start, Stop, Remove, Restart, Here, Eval},
 	Authors: []*cli.Author{
 		{
 			Name:  "Shindou Mihou",
